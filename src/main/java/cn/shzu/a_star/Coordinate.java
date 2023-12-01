49c8a75eaa23db2091b464e76b99e1dfd7e51eea
soya <2820116913@qq.com>
2023-12-01 18:48:30 +0800

    封装坐标类

---
+++ src/main/java/cn/shzu/a_star/Coordinate.java
@@ -0,0 +1,49 @@
+package cn.shzu.a_star;
+
+import java.util.Objects;
+
+/**
+ * @author soya
+ * @version 1.0
+ * @project A_starOnDem
+ * @description point coord
+ * @date 2023/11/14 23:07:39
+ */
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinate)) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
